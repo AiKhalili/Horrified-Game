70137caa2e127b97a148662fb63224ca8bbdf82c
AiKhalili <aisakhalili760@gmail.com>
2025-06-13 10:37:50 +0330

    fix the bug of onstructor

---
+++ include/Villager.hpp
@@ -0,0 +1,29 @@
+#ifndef VILLAGER_HPP
+#define VILLAGER_HPP
+
+#include <string>
+#include "Location.hpp"
+
+class Villager
+{
+private:
+    std::string name;
+    Location *currentLocation;
+    Location *safeLocation;
+    bool alive = true;
+    bool rescue = false;
+
+public:
+    Villager(const std::string &, Location *, Location *);
+    std::string getName() const;
+    Location *getCurrentLocation() const; // گرفتن مکان فعلی محلی
+    Location *getSafeLocation() const;    // گرفتن مکان امن محلی
+    void setLocation(Location *);         // تغییر مکان محلی مثلا بعد از Move یا Guide
+    bool isSafeLocation() const;
+    bool isAlive() const;
+    void kill();
+    void markRescued();
+    bool isRescued() const;
+};
+
+#endif
---
+++ src/Villager.cpp
@@ -0,0 +1,56 @@
+#include "Villager.hpp"
+
+using namespace std;
+
+Villager::Villager(const std::string &name, Location *start, Location *safeLocation)
+    : name(name),
+      currentLocation(start),
+      safeLocation(safeLocation),
+      alive(true),
+      rescue(false) {}
+
+string Villager::getName() const
+{
+    return name;
+}
+
+Location *Villager::getCurrentLocation() const
+{
+    return currentLocation;
+}
+
+Location *Villager::getSafeLocation() const
+{
+    return safeLocation;
+}
+
+void Villager::setLocation(Location *newLocation)
+{
+    currentLocation = newLocation;
+}
+
+bool Villager::isSafeLocation() const
+{
+    return currentLocation == safeLocation;
+}
+
+bool Villager::isAlive() const
+{
+    return alive;
+}
+
+void Villager::kill()
+{
+    alive = false;
+}
+
+void Villager::markRescued()
+{
+    rescue = true;
+    alive = false;
+}
+
+bool Villager::isRescued() const
+{
+    return rescue;
+}
