da8b17fb286eba3103806c2f085584202a12c129
AiKhalili <aisakhalili760@gmail.com>
2025-06-24 14:19:16 +0330

    add the latest version of Mayor class

--- include/Mayor.hpp
+++ include/Mayor.hpp
@@ -2,13 +2,14 @@
 #define MAYOR_HPP
 
 #include "Hero.hpp"
-#include "Map.hpp"
+
+class Map;
 
 class Mayor : public Hero
 {
 public:
     Mayor(const std::string &, Map *);
-    void specialAction(const std::vector<Item *> &) override;
+    void specialAction(const std::vector<Item *> &) override; 
     std::string getClassName() const override;
 };
 
--- src/Mayor.cpp
+++ src/Mayor.cpp
@@ -1,14 +1,18 @@
+#include "Hero.hpp"
 #include "Mayor.hpp"
+#include "Map.hpp"
 #include <iostream>
 
 using namespace std;
 
+class Map;
+
 Mayor::Mayor(const std::string &name, Map *map)
-    : Hero(name, map->getLocation("Theater"), 4) {}
+    : Hero(name, map ? map->getLocation("Theatre") : throw GameException("Map is null!"), 5) {}
 
 void Mayor::specialAction(const vector<Item *> &)
 {
-    tui.showMessageScreen("Mayor has no special ability!");
+    cout << "Mayor has no special ability!\n";
 }
 
 string Mayor::getClassName() const
