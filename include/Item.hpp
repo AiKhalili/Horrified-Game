3a228b311c9a8a71e1ce6ee81d87fbe810653986
AiKhalili <aisakhalili760@gmail.com>
2025-06-24 14:35:55 +0330

    Add the pickedFrom variable and its methods for the invisible man

--- include/Item.hpp
+++ include/Item.hpp
@@ -4,22 +4,31 @@
 
 class Location;
 
-enum class Color{red, blue, yellow};
+enum class COlOR
+{
+    red,
+    blue,
+    yellow
+};
 
-class Item{
-    private:
-    int strength; //قدرت
-    Color color; // سه رنگ ابی قرمز زرد
-    std::string name; // اسمش
-    Location* location; // مکانی که باید قرارش بدیم
+class Item
+{
+private:
+    int strength;
+    COlOR color;
+    std::string name;
+    Location *location;     // مکانی که باید قرارش بدیم
+    std::string pickedFrom; // آیتم از کجا برداشته شده
 
-    public:
-    Item(int, Color, std::string, Location*);
-    int get_strength()const;
-    Color get_color()const;
-    std::string get_name()const;
-    Location* get_location()const;
-    void set_location(Location*);
-    
+public:
+    Item(int, COlOR, std::string, Location *);
+    int get_strength() const;
+    COlOR get_color() const;
+    std::string get_color_to_string();
+    std::string get_name() const;
+    Location *get_location() const;
+    std::string get_pickedFrom() const;
+    void set_location(Location *);
+    void set_pickedFrom(const std::string &);
 };
 #endif
--- src/Item.cpp
+++ src/Item.cpp
@@ -1,15 +1,48 @@
 #include "Item.hpp"
+#include "Location.hpp"
 #include <iostream>
 using namespace std;
 
-Item::Item(int strength, Color color, std::string name, Location* location){
+Item::Item(int strength, COlOR color, string name, Location *location)
+{
     this->strength = strength;
     this->color = color;
     this->name = name;
     this->location = location;
+    if (location)
+    {
+        this->pickedFrom = location->get_name();
+    }
 }
-int Item::get_strength()const{return strength;}
-Color Item::get_color()const{return color;}
-std::string Item::get_name()const{return name;}
-Location* Item:: get_location()const{return location;}
-void Item::set_location(Location* location){this->location = location;}
+
+int Item::get_strength() const { return strength; }
+COlOR Item::get_color() const { return color; }
+string Item::get_name() const { return name; }
+Location *Item::get_location() const { return location; }
+void Item::set_location(Location *location) { this->location = location; }
+
+void Item::set_pickedFrom(const string &loc)
+{
+    pickedFrom = loc;
+}
+
+string Item::get_pickedFrom() const
+{
+    return pickedFrom;
+}
+
+string Item::get_color_to_string()
+{
+    if (color == COlOR::red)
+    {
+        return "red";
+    }
+    else if (color == COlOR::blue)
+    {
+        return "blue";
+    }
+    else
+    {
+        return "yellow";
+    }
+}
