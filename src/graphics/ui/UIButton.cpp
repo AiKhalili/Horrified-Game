7b2d50844fce0c332a86fb76b43e02ec9dc209ce
AiKhalili <aisakhalili760@gmail.com>
2025-07-13 22:29:35 +0330

    Add UIButton header

---
+++ src/graphics/ui/UIButton.cpp
@@ -0,0 +1,81 @@
+#include "graphics/ui/UIButton.hpp"
+
+UIButton::UIButton(Rectangle bds, const std::string &txt,
+                   int sz, Color txtColor,
+                   Color bg, Color hover)
+    : bounds(bds), text(txt), fontSize(sz),
+      textColor(txtColor), bgColor(bg), hoverColor(hover) {}
+
+void UIButton::setOnClick(std::function<void()> callback)
+{ // تابع روی کلید به متغیر onClick انتساب داده میشه
+    onClick = callback;
+}
+
+void UIButton::setFocus(bool f)
+{
+    isFocused = f;
+}
+
+void UIButton::setFont(Font f){
+    font = f;
+    useCustomFont = true;
+}
+
+void UIButton::click()
+{
+    if (onClick)
+    {
+        onClick();
+    }
+}
+
+void UIButton::update()
+{
+    Vector2 mousePos = GetMousePosition();
+    isHovered = CheckCollisionPointRec(mousePos, bounds);
+
+    if (isHovered && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    { // اگه موس روی دکمه بود و کاربر چپ کلیک کرد
+        if (onClick)
+        {
+            onClick();
+        }
+    }
+}
+
+void UIButton::render()
+{
+    Color currentColor = isHovered ? hoverColor : bgColor;
+
+    DrawRectangleRec(bounds, currentColor); // رسم مستطیل پس زمینه
+
+    if (isFocused)
+    { // کشیدن کادر رنگی دور مستطیل
+        DrawRectangleLinesEx(bounds, 3, RED);
+    }
+
+    Vector2 textSize; // اندازی گیری سایز متن برای اینکه دقیقا وسط کادر باشه
+    if (useCustomFont)
+    {
+        textSize = MeasureTextEx(font, text.c_str(), (float)fontSize, 2);
+    }
+    else
+    {
+        int w = MeasureText(text.c_str(), fontSize); // ارتفاع
+        textSize = Vector2{(float)w, (float)fontSize};
+    }
+
+    // محاسبه محل رسم متن
+    Vector2 textPos = {bounds.x + (bounds.width - textSize.x) / 2,
+                       bounds.y + (bounds.height - textSize.y) / 2};
+
+    // رسم متن
+    if (useCustomFont)
+    {
+        DrawTextEx(font, text.c_str(), textPos, (float)fontSize, 2, textColor);
+    }
+    else
+    {
+        DrawText(text.c_str(), (int)textPos.x, (int)textPos.y, fontSize, textColor);
+    }
+}
