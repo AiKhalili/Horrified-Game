61ae629b90e78ae86b3ec05365a079c986b8e58a
AiKhalili <aisakhalili760@gmail.com>
2025-07-14 23:12:19 +0330

    Add the new methods for timer in label class

--- src/graphics/ui/UILabel.cpp
+++ src/graphics/ui/UILabel.cpp
@@ -3,10 +3,11 @@
 
 using namespace std;
 
-UILabel::UILabel(Vector2 pos, const string &txt, int sz, Color clr) : position(pos),
-                                                                      text(txt),
-                                                                      fontSize(sz),
-                                                                      color(clr) {}
+UILabel::UILabel(Vector2 pos, const string &txt, int sz, Color clr, float dur) : position(pos),
+                                                                                 text(txt),
+                                                                                 fontSize(sz),
+                                                                                 color(clr),
+                                                                                 duration(dur) {}
 
 void UILabel::setFont(Font f)
 {
@@ -14,8 +15,20 @@ void UILabel::setFont(Font f)
     useCustomFont = true;
 }
 
+void UILabel::update()
+{
+    if (duration > 0.0f)
+    {
+        timer += GetFrameTime();
+    }
+}
+
 void UILabel::render()
 {
+    if (duration > 0.0f && timer >= duration)
+    {
+        return;
+    }
     if (useCustomFont)
     {
         DrawTextEx(font, text.c_str(), position, (float)fontSize, 2, color);
@@ -24,4 +37,9 @@ void UILabel::render()
     {
         DrawText(text.c_str(), position.x, position.y, fontSize, color);
     }
+}
+
+bool UILabel::isExoired() const
+{
+    return (duration > 0.0f && timer >= duration);
 }
