bbf2cbd62579f1a314c76cd1b1f346d7e0286236
ftmshab <ftmshbn76395.gmailcom>
2025-07-15 23:23:23 +0330

    Add sections to save at least 5 games and launch any existing game from where it left off

--- include/core/Dracula.hpp
+++ include/core/Dracula.hpp
@@ -13,19 +13,21 @@ class Hero;
 class Dracula : public Monster
 {
 private:
-    std::map<std::string, bool> coffins; // مپ برای تابوت های دراکولا
+    std::map<std::string, bool> coffins;
 
 public:
     Dracula();
     virtual std::vector<Location *> getAdvanceLocation() override;
     virtual void advanceMission(Location *) override;
     virtual bool canbedefeated() const override;
-    void specialPower(Hero*) override; // استفاده از قدرت برای هیولا
+    void specialPower(Hero*) override;
     virtual std::vector<Item> getAdvanceRequirement() const override;
     virtual std::vector<Item> getDefeatRequirement() const override;
     virtual int getCounter() const override;
     virtual bool isAdvanceLocation(const std::string&) const override;
     virtual std::string serialize() const override;
+    void setCoffins(const std::map<std::string, bool>& newCoffins);
+
 };
 
 #endif
