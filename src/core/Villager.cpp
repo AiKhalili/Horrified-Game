8d8b9634a37f0d7e1b0eecb5cb42f1d16d060611
ftmshab <ftmshbn76395.gmailcom>
2025-07-15 23:44:18 +0330

    Add sections to save at least 5 games and launch any existing game from where it left off

--- src/core/Villager.cpp
+++ src/core/Villager.cpp
@@ -1,4 +1,8 @@
 #include "core/Villager.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "core/Map.hpp"
 
 using namespace std;
 
@@ -53,4 +57,64 @@ void Villager::markRescued()
 bool Villager::isRescued() const
 {
     return rescue;
+}
+
+string Villager::serialize() const
+{
+    string data = "Villager|";
+    data += name + "|";
+    data += (currentLocation ? currentLocation->get_name() : "null") + "|";
+    data += (safeLocation ? safeLocation->get_name() : "null") + "|";
+    data += (alive ? "1|" : "0|");
+    data += (rescue ? "1" : "0");
+
+
+    return data;
+}
+
+Villager* Villager::deserialize(const std::string& line)
+{
+    stringstream ss(line);
+    string part;
+
+    getline(ss, part, '|'); // "Villager"
+    getline(ss, part, '|');
+    string name = part;
+
+    getline(ss, part, '|');
+    Location* current = (part == "null") ? nullptr : Map::get_instanse()->getLocation(part);
+
+    getline(ss, part, '|');
+    Location* safe = (part == "null") ? nullptr : Map::get_instanse()->getLocation(part);
+
+    getline(ss, part, '|');
+    bool alive = (part == "1");
+
+    getline(ss, part, '|');
+    bool rescued = (part == "1");
+
+    Villager* v = new Villager(name, current, safe);
+
+    if (current != nullptr)
+    {
+        Map::get_instanse()->addVillager(current->get_name(), v);
+    }
+    else
+    {
+        v->setLocation(nullptr);
+    }
+
+    if (!alive)
+    {
+        v->kill();
+        v->setLocation(nullptr);
+        
+        if (current != nullptr)
+            Map::get_instanse()->removeVillagerFrom(current->get_name(), v);
+    }
+
+    if (rescued)
+        v->markRescued();
+
+    return v;
 }
