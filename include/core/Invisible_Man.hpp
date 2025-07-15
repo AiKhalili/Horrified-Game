ec97dafc22427d1beebfe774f57fa0dbfed579f1
ftmshab <ftmshbn76395.gmailcom>
2025-07-15 23:25:46 +0330

    Add sections to save at least 5 games and launch any existing game from where it left off

--- include/core/Invisible_Man.hpp
+++ include/core/Invisible_Man.hpp
@@ -13,7 +13,7 @@ class Villager;
 class Invisible_Man : public Monster
 {
 private:
-    std::map<std::string, bool> evidence; // مپ برای نگهداری جاهایی که مدارک مبنی بر وجود مردنامريی قرار داره
+    std::map<std::string, bool> evidence;
     std::map<std::string, bool> advanceTracker; // برای مشخص کردن پیشرفت مأموریت در هر مکان (تکمیل یا ناقص) است
 
 public:
@@ -21,12 +21,14 @@ public:
     virtual std::vector<Location *> getAdvanceLocation() override;
     virtual void advanceMission(Location *) override;
     virtual bool canbedefeated() const override;
-    void specialPower(Hero *) override; // استفاده از قدرت برای هیولا
+    void specialPower(Hero *) override;
     virtual std::vector<Item> getAdvanceRequirement() const override;
     virtual std::vector<Item> getDefeatRequirement() const override;
     virtual int getCounter() const override;
     virtual bool isAdvanceLocation(const std::string &) const override;
     virtual std::string serialize() const override;
+    void setEvidence(const std::map<std::string, bool>& newEvidence);
+
 };
 
 #endif
