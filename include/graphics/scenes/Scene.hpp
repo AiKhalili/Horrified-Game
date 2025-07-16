42c39a7939c4d77be74807f1c341ff6ae93bea3e
AiKhalili <aisakhalili760@gmail.com>
2025-07-16 14:24:59 +0330

    Add 2 new methods for the secenes

--- include/graphics/scenes/Scene.hpp
+++ include/graphics/scenes/Scene.hpp
@@ -1,10 +1,13 @@
 #ifndef SCENE_HPP
 #define SCENE_HPP
 
-class Scene{
+class Scene
+{
 public:
     virtual void update(float deleteTime) = 0;
     virtual void render() = 0;
+    virtual void onEnter() {} // when you enter the scene
+    virtual void inExit() {}  // when you exit the scene
     virtual ~Scene() = default;
 };
 
