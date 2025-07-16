4787e31cc8d246a228e8754257ff9698736fa16e
ftmshab <ftmshbn76395.gmailcom>
2025-07-16 13:28:04 +0330

    Adding a Save Manager class to control the sections related to saving and loading the requested games.

---
+++ include/saves/SaveManager.hpp
@@ -0,0 +1,71 @@
+#ifndef SAVEMANAGER_HPP
+#define SAVEMANAGER_HPP
+
+#include <string>
+#include <vector>
+#include "core/Hero.hpp"
+#include "core/Mayor.hpp"
+#include "core/Archaeologist.hpp"
+#include "core/PerkCard.hpp"
+#include "core/Monster.hpp"
+#include "core/Dracula.hpp"
+#include "core/Invisible_Man.hpp"
+#include "core/MonsterCard.hpp"
+#include "core/Villager.hpp"
+#include "core/Location.hpp"
+#include "core/Map.hpp"
+#include "core/Item.hpp"
+#include "core/GameException.hpp"
+
+class Game;
+class Map;
+
+class SaveManager
+{
+
+private:
+
+public:
+
+    void saveGameToSlot(int, const Game&); // ذخیره کامل بازی با شماره اسلات
+    void loadGameFromSlot(int, Game&); // بارگذاری بازی با شماره اسلاتی که کاربر وارد کرده و میخواد بازی رو ادامه بده
+
+    void saveHeroesToFile(const std::string&, const std::vector<Hero*>& ); // ذخیره کردن قهرمان فعال بازی
+    std::vector<Hero*> loadHeroesFromFile(const std::string&); // خواندن اطلاعات قهرمان و ساختن آن بر اساس اطاعات قبلی برای ادامه بازی
+
+    void saveCurrentHeroStateToFile(const std::string& filename, int currentIndex, int actionsLeft);
+    std::pair<int, int> loadCurrentHeroStateFromFile(const std::string& filename);
+    
+    void saveMonstersToFile(const std::string&, const std::vector<Monster*>&);
+    std::vector<Monster*> loadMonstersFromFile(const std::string& filename);
+
+    void saveVillagersToFile(const std::string&, const std::vector<Villager*>&);
+    std::vector<Villager*> loadVillagersFromFile(const std::string&);
+
+    void saveItemsToFile(const std::string&, const std::vector<Item*>&);
+    std::vector<Item*> loadItemsFromFile(const std::string&);
+
+    void saveMonsterCardsToFile(const std::string& , const std::vector<MonsterCard>&);
+    std::vector<MonsterCard> loadMonsterCardsFromFile(const std::string&);
+
+    void saveItemManagerStateToFile(const std::string&);
+    void loadItemManagerStateFromFile(const std::string&);
+
+    void saveTerrorLevel(const std::string&, int);
+    int loadTerrorLevel(const std::string&);
+
+    void saveCurrentTurn(const std::string&, int);
+    int loadCurrentTurn(const std::string&);
+
+    void saveTimes(const std::string&, int, int);
+    void loadTimes(const std::string&, int&, int&);
+
+    void saveFrenzy(const std::string& , const std::string&);
+    Monster* loadFrenzy(const std::string&, const std::vector<Monster*>&);
+
+    void savePerkDeckToFile(const std::string& filename, const std::vector<PerkCard>& deck);
+    std::vector<PerkCard> loadPerkDeckFromFile(const std::string& filename);
+
+};
+
+#endif
