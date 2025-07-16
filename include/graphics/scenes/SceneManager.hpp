7abf8783dea9793b232c4bbb00a347debff74608
AiKhalili <aisakhalili760@gmail.com>
2025-07-16 14:27:25 +0330

    Add a class to manage the scenes

---
+++ include/graphics/scenes/SceneManager.hpp
@@ -0,0 +1,28 @@
+#ifndef SCENEMANAGER_HPP
+#define SCENEMANAGER_HPP
+
+#include <unordered_map>
+#include <memory>
+#include <string>
+#include "graphics/scenes/Scene.hpp"
+
+class SceneManager
+{
+public:
+    static SceneManager &getInstance();
+    void registerScene(const std::string &name, std::unique_ptr<Scene> scene); // register the scene with a unique name
+    void goTo(const std::string &name);                                        // change the scene
+
+    // call in *main*
+    void update(float deleteTime);
+    void render();
+
+private:
+    SceneManager() = default;
+    SceneManager(const SceneManager &) = delete;
+    SceneManager &operator=(const SceneManager &) = delete;
+    std::unordered_map<std::string, std::unique_ptr<Scene>> scenes; // all of the scenes
+    Scene *currentScene = nullptr;                                  // the current scene
+};
+
+#endif
