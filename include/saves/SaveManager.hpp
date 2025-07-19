955a7eea9d2ad72e6f23ec63b0767982fa454947
ftmshab <ftmshbn76395.gmailcom>
2025-07-19 20:37:43 +0330

    Adding sections that store the exact time the game was saved in the executable files

--- include/saves/SaveManager.hpp
+++ include/saves/SaveManager.hpp
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <vector>
+#include <ctime>
+#include <iomanip>
+#include <utility>
 #include "core/Hero.hpp"
 #include "core/Mayor.hpp"
 #include "core/Archaeologist.hpp"
@@ -27,17 +30,17 @@ private:
 
 public:
 
-    void saveGameToSlot(int, const Game&); // ذخیره کامل بازی با شماره اسلات
+    void saveGameToSlot(int, Game&); // ذخیره کامل بازی با شماره اسلات
     void loadGameFromSlot(int, Game&); // بارگذاری بازی با شماره اسلاتی که کاربر وارد کرده و میخواد بازی رو ادامه بده
 
     void saveHeroesToFile(const std::string&, const std::vector<Hero*>& ); // ذخیره کردن قهرمان فعال بازی
     std::vector<Hero*> loadHeroesFromFile(const std::string&); // خواندن اطلاعات قهرمان و ساختن آن بر اساس اطاعات قبلی برای ادامه بازی
 
-    void saveCurrentHeroStateToFile(const std::string& filename, int currentIndex, int actionsLeft);
-    std::pair<int, int> loadCurrentHeroStateFromFile(const std::string& filename);
+    void saveCurrentHeroStateToFile(const std::string& , int, int);
+    std::pair<int, int> loadCurrentHeroStateFromFile(const std::string&);
     
     void saveMonstersToFile(const std::string&, const std::vector<Monster*>&);
-    std::vector<Monster*> loadMonstersFromFile(const std::string& filename);
+    std::vector<Monster*> loadMonstersFromFile(const std::string&);
 
     void saveVillagersToFile(const std::string&, const std::vector<Villager*>&);
     std::vector<Villager*> loadVillagersFromFile(const std::string&);
@@ -63,8 +66,13 @@ public:
     void saveFrenzy(const std::string& , const std::string&);
     Monster* loadFrenzy(const std::string&, const std::vector<Monster*>&);
 
-    void savePerkDeckToFile(const std::string& filename, const std::vector<PerkCard>& deck);
-    std::vector<PerkCard> loadPerkDeckFromFile(const std::string& filename);
+    void savePerkDeckToFile(const std::string&, const std::vector<PerkCard>&);
+    std::vector<PerkCard> loadPerkDeckFromFile(const std::string&);
+
+    void writeSaveTimestamp(const std::string&);
+    std::string readSaveTimestamp(const std::string& );
+
+    std::vector<std::pair<int, std::string>> getAllSaveSlotsWithTimestamps();
 
 };
 
