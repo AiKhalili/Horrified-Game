ab1867cf086742b7313cff4875b4ab88449cdd65
AiKhalili <aisakhalili760@gmail.com>
2025-07-20 19:34:13 +0330

    add load games class

---
+++ include/graphics/scenes/LoadGamesScene.hpp
@@ -0,0 +1,25 @@
+#ifndef SHOWLOADGAMESSCENE_HPP
+#define SHOWLOADGAMESSCENE_HPP
+
+#include "graphics/scenes/Scene.hpp"
+#include "graphics/ui/UIManager.hpp"
+#include "raylib.h"
+
+class LoadGamesScene : public Scene
+{
+private:
+    Texture2D background;
+    Font normalFont;
+    Font spooky;
+    UIManager ui;
+
+    void createButtons();
+
+public:
+    void onEnter() override;
+    void onExit() override;
+    void update(float deleteTime) override;
+    void render() override;
+};
+
+#endif
