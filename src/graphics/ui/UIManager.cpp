577e8759f73564eef6fb4df65afc87c9a77ac97d
AiKhalili <aisakhalili760@gmail.com>
2025-07-21 14:19:18 +0330

    change the update() func

--- src/graphics/ui/UIManager.cpp
+++ src/graphics/ui/UIManager.cpp
@@ -3,41 +3,101 @@
 void UIManager::add(std::unique_ptr<UIElement> element)
 {
     if (auto *btn = dynamic_cast<UIButton *>(element.get()))
-    {
-        buttons.push_back(btn);
-        if (selectedIndex == -1)
-        {
+    {                           // check if the element if button
+        buttons.push_back(btn); // add button to internal button list
+        if (focusMode == FocusMode::None)
+        {                      // if no element is focused yet, set focus to the fist button
             selectedIndex = 0; // choose the first button
             btn->setFocus(true);
+            focusMode = FocusMode::Button;
         }
     }
+    else if (auto *box = dynamic_cast<UIInputBox *>(element.get()))
+    { // if no a button , check if it's an input box
+        inputBoxes.push_back(box);
+        if (focusMode == FocusMode::None)
+        {
+            selectedInputIndex = 0;
+            box->setFocus(true);
+            focusMode = FocusMode::Input;
+        }
+    }
+
+    // store the element in the master list for rendering and updating
     elements.push_back(std::move(element));
 }
 
 void UIManager::update()
 {
-    if (!buttons.empty())
-    { // only when there is at least one button
+    if (IsKeyPressed(KEY_TAB))
+    { // handle switching focus between buttons and inputBoxes using TAB
+        if (focusMode == FocusMode::Button && !inputBoxes.empty())
+        {
+            buttons[selectedIndex]->setFocus(false);
+
+            if (selectedInputIndex < 0 || selectedInputIndex >= inputBoxes.size())
+            {
+                selectedInputIndex = 0;
+            }
+
+            inputBoxes[selectedInputIndex]->setFocus(true);
+            focusMode = FocusMode::Input;
+        }
+        else if (focusMode == FocusMode::Input && !buttons.empty())
+        {
+            inputBoxes[selectedInputIndex]->setFocus(false);
+
+            if (selectedIndex < 0 || selectedIndex >= buttons.size())
+            {
+                selectedIndex = 0;
+            }
+            buttons[selectedIndex]->setFocus(true);
+            focusMode = FocusMode::Button;
+        }
+    }
+
+    if (focusMode == FocusMode::Input && !inputBoxes.empty())
+    { // navigate between input boxes using arrow keys
         if (IsKeyPressed(KEY_DOWN))
-        { // the down key = going to the next one
+        {
+            inputBoxes[selectedInputIndex]->setFocus(false);
+            selectedInputIndex = (selectedInputIndex + 1) % inputBoxes.size();
+            inputBoxes[selectedInputIndex]->setFocus(true);
+        }
+        if (IsKeyPressed(KEY_UP))
+        {
+            inputBoxes[selectedInputIndex]->setFocus(false);
+            selectedInputIndex = (selectedInputIndex - 1 + inputBoxes.size()) % inputBoxes.size();
+            inputBoxes[selectedInputIndex]->setFocus(true);
+        }
+    }
+
+    if (focusMode == FocusMode::Button && !buttons.empty())
+    {
+        if (IsKeyPressed(KEY_DOWN))
+        {
             buttons[selectedIndex]->setFocus(false);
             selectedIndex = (selectedIndex + 1) % buttons.size();
             buttons[selectedIndex]->setFocus(true);
         }
         if (IsKeyPressed(KEY_UP))
-        { // the up key = going to the previous key
+        {
             buttons[selectedIndex]->setFocus(false);
             selectedIndex = (selectedIndex - 1 + buttons.size()) % buttons.size();
             buttons[selectedIndex]->setFocus(true);
         }
-        if (IsKeyPressed(KEY_ENTER) || IsKeyPressed(KEY_SPACE))
-        { // when Enter or Space is pressed, perform a click action to run the button's function
+        if (IsKeyPressed(KEY_ENTER))
+        {
             buttons[selectedIndex]->click();
         }
     }
+
     for (auto &e : elements)
-    { // update all of the elements
-        e->update();
+    { // update all UI elements regardless of focus
+        if (e)
+        {
+            e->update();
+        }
     }
 }
 
@@ -45,7 +105,10 @@ void UIManager::render()
 { // render all of the elements
     for (auto &e : elements)
     {
-        e->render();
+        if (e)
+        {
+            e->render();
+        }
     }
 }
 
@@ -53,5 +116,8 @@ void UIManager::clear()
 { // clear all widgets and reset focus state
     elements.clear();
     buttons.clear();
+    inputBoxes.clear();
+    selectedInputIndex = -1;
     selectedIndex = -1;
+    focusMode = FocusMode::None;
 }
