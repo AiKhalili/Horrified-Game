5c8e2834d7344b51bb7107d4b32ff7ff71442a3d
AiKhalili <aisakhalili760@gmail.com>
2025-07-21 14:14:22 +0330

    adding the focus mode to the ui manager class

--- include/graphics/ui/UIManager.hpp
+++ include/graphics/ui/UIManager.hpp
@@ -5,7 +5,15 @@
 #include <memory>
 #include "graphics/ui/UIElement.hpp"
 #include "graphics/ui/UIButton.hpp"
+#include "graphics/ui/UIInputBox.hpp"
 #include "raylib.h"
+
+enum class FocusMode
+{
+    None,
+    Button,
+    Input
+};
 
 class UIManager
 {
@@ -17,8 +25,11 @@ public:
 
 private:
     std::vector<std::unique_ptr<UIElement>> elements; // all of the elements : buttons and labels
-    std::vector<UIButton *> buttons; // just buttons
-    int selectedIndex = -1; // By default, no button is selected(-1)
+    std::vector<UIButton *> buttons;                  // just buttons
+    std::vector<UIInputBox *> inputBoxes;             // just input boxes
+    int selectedIndex = -1;                            // By default, no button is selected(-1)
+    int selectedInputIndex = -1;                      // By default, no input is selected(-1)
+    FocusMode focusMode = FocusMode::None;
 };
 
 #endif
