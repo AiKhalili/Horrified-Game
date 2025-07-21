4ddd1506538220615c74023d55c1da38839c28e7
AiKhalili <aisakhalili760@gmail.com>
2025-07-21 11:31:00 +0330

    Adding an outer border in label class

--- include/graphics/ui/UILabel.hpp
+++ include/graphics/ui/UILabel.hpp
@@ -8,21 +8,26 @@
 class UILabel : public UIElement
 {
 private:
-    Vector2 position;           // موقیت متن
-    std::string text;           // خود متنی که قراره نمایش داده بشه
-    int fontSize;               // اندازه فونت
-    Color color;                // رنگ متن
+    Vector2 position; // موقیت متن
+    std::string text; // خود متنی که قراره نمایش داده بشه
+    int fontSize;     // اندازه فونت
+    Color color;      // رنگ متن
+    Color backgroundColor;
     Font font;                  // فونت متن
     bool useCustomFont = false; // آیا از فونت سفارشی استفاده بشه یا نه؟
-    float duration = 0.0f;      // چند ثانیه نمایش داده بشه
-    float timer = 0.0f;         // شمارنده زمان سپری شده
+    bool drawBackground = false;
+    float duration;     // چند ثانیه نمایش داده بشه
+    float timer = 0.0f; // شمارنده زمان سپری شده
+    float padding;      // فاصله بین متن و کادر
 
 public:
-    UILabel(Vector2 pos, const std::string &text, int fontSize = 20, Color color = BLACK, float duration = 0.0f);
+    UILabel(Vector2 pos, const std::string &text, int fontSize = 20,
+            Color color = BLACK, float duration = 0.0f);
     void render() override;
     void update() override;
     void setFont(Font);
     bool isExoired() const;
+    void enableBackground(Color bgColor, float pad = 10.0f);
 };
 
 #endif
