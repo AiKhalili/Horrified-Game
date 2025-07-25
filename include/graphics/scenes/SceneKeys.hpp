977ecbfcb7958505c0102ceadf45e269f6f08f5f
AiKhalili <aisakhalili760@gmail.com>
2025-07-25 19:39:51 +0330

    adding scene keys as namespace

---
+++ include/graphics/scenes/SceneKeys.hpp
@@ -0,0 +1,21 @@
+#ifndef SCENEKEYS_HPP
+#define SCENEKEYS_HPP
+
+#include <string>
+
+namespace SceneKeys
+{
+    inline const std::string INTRO_SCENE = "IntroScene";
+    inline const std::string MAIN_MENU_SCENE = "MainMenuScene";
+    inline const std::string LOAD_GAMES_SCENE = "LoadGamesScene";
+    inline const std::string BOARD_SCENE = "BoardScene";
+    inline const std::string PLAYER_INFO_SCENE = "PlayerInfoScene";
+    inline const std::string HERO_SELECTION_SCENE = "HeroSelectionScene";
+    inline const std::string GAME_WON_SCENE = "GameWonScene";
+    inline const std::string GAME_LOST_SCENE = "GameLostScene";
+    inline const std::string OUT_OF_TIME_SCENE = "OutOfTimeScene";
+    inline const std::string PERK_CARD_SCENE = "PerkCardScene";
+    inline const std::string MONSTER_PHASE_SCENE = "MonsterPhaseScene";
+}
+
+#endif
