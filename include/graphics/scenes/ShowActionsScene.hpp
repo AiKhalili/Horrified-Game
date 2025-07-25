71d47a33a557c4e47bd76d84780803bd1f0eda63
ftmshab <ftmshbn76395.gmailcom>
2025-07-25 23:18:07 +0330

    The initial code of the hero action selection page

---
+++ include/graphics/scenes/ShowActionsScene.hpp
@@ -0,0 +1,34 @@
+#ifndef SHOWACTIONSSCENE_HPP
+#define SHOWACTIONSSCENE_HPP
+
+#include "graphics/scenes/Scene.hpp"
+#include "graphics/ui/UIManager.hpp"
+#include "raylib.h"
+
+class ShowActionsScene : public Scene
+{
+
+private:
+    UIManager ui;
+    Texture2D background;
+    Font font;
+    void craeteLabels();
+    void createButtons();
+
+public:
+ void onEnter() override;
+    void onExit() override;
+    void update(float deltaTime) override;
+    void render() override;
+
+    void openMoveAction();
+    void openPickUpAction();
+    void openDefeatAction();
+    void openHelpAction();
+    void openSpecialAction();
+    void openGuideAction();
+    void openAdvanceAction();
+    void openUsePerkAction();
+};
+
+#endif
