e76a7fd9dbe38b24cfb5bde62190661ce7509daa
AiKhalili <aisakhalili760@gmail.com>
2025-07-25 19:41:44 +0330

    add game app and handle Exit of game

---
+++ src/app/GameApp.cpp
@@ -0,0 +1,28 @@
+#include "app/GameApp.hpp"
+#include "raylib.h"
+#include "audio/AudioManager.hpp"
+#include "graphics/scenes/SceneManager.hpp"
+#include "graphics/scenes/SceneKeys.hpp"
+
+void GameApp::run()
+{
+    InitWindow(1600, 900, "HORRIFIED");
+    SetTargetFPS(60);
+    AudioManager::getInstance().init();
+
+    SceneManager &sm = SceneManager::getInstance();
+    sm.registerAllScenes();
+    sm.goTo(SceneKeys::INTRO_SCENE);
+
+    while (!WindowShouldClose() && !sm.isExitRequested())
+    {
+        float dt = GetFrameTime();
+        sm.update(dt);
+        BeginDrawing();
+        sm.render();
+        EndDrawing();
+    }
+
+    AudioManager::getInstance().dispose();
+    CloseWindow();
+}
