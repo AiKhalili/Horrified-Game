0bffb2b6c77fde38e1577a1a2948501421353885
AiKhalili <aisakhalili760@gmail.com>
2025-07-27 12:39:02 +0330

    adding the drawMonstersGlow() to the board scene

--- include/graphics/scenes/BoardScene.hpp
+++ include/graphics/scenes/BoardScene.hpp
@@ -37,6 +37,7 @@ private:
     void drawHeroInfo();
     void drawHeroPositionMarker();
     void drawGlow();
+    void drawMonsterGlow();
     void drawHeroActions();
 
     void handleSelectAction();
