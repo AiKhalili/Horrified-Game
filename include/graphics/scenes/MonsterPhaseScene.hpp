0ac46c04fd18093059bcb53c37fe8f122a46693b
AiKhalili <aisakhalili760@gmail.com>
2025-07-31 13:51:01 +0330

    adding the steps functions

--- include/graphics/scenes/MonsterPhaseScene.hpp
+++ include/graphics/scenes/MonsterPhaseScene.hpp
@@ -11,7 +11,8 @@ enum class MonsterPhaseStep
 {
     CheckMonsterPhasePerk,
     DrawMonsterCard,
-    PlaceItems
+    PlaceItems,
+    RunEvent
 };
 
 class MonsterPhaseScene : public Scene
@@ -19,23 +20,41 @@ class MonsterPhaseScene : public Scene
 private:
     Texture2D background;
     Texture2D cardTexture;
-    Font font;
+    Texture2D bats;
+    Font normalFont;
+    Font spookyFont;
+
+    MonsterCard card;
+    bool cardDrawn = false;
+    float cardScale = 0.0f;
+    Vector2 cardPos = {0, 0};
+
     UIManager ui;
     Game &game = Game::getInstance();
+
     MonsterPhaseStep currentStep = MonsterPhaseStep::CheckMonsterPhasePerk;
-
     float stepTimer = 0.0f;
-    float cardScale = 0.0f;
-
-    bool cardDrawn = false;
     bool skipPhase = false;
-    MonsterCard card;
 
-    std::vector<Item*> items;
+    std::vector<Item *> items;
     std::vector<Texture2D> itemTex;
+    bool itemsPlaced = false;
+    float itemsTimer = 0.0f;
+    float itemsAlpha = 0.0f;
+    bool showItemIcons = false;
+
+    void showMessage(const std::string& text,Vector2 pos,int fontSize,float time);
 
     void step_CheckMonsterPhasePerk(float deleteTime);
     void step_DrawMonsterCard(float deleteTime);
+    void step_PlaceItems(float deleteTime);
+    void step_RunEvent(float deleteTime);
+
+    void renderBackGround();
+    void renderMonsterCard();
+    void renderItems();
+
+    void render_FormOfTheBat(float deleteTime);
 
 public:
     void onEnter() override;
