dd43b114e41806694a96a5e6a1c083952353748f
ftmshab <ftmshbn76395.gmailcom>
2025-08-01 12:16:00 +0330

    Changes related to saving and loading for better and easier access

--- include/core/Dracula.hpp
+++ include/core/Dracula.hpp
@@ -20,14 +20,16 @@ public:
     virtual std::vector<Location *> getAdvanceLocation() override;
     virtual void advanceMission(Location *) override;
     virtual bool canbedefeated() const override;
-    void specialPower(Hero*) override;
+    void specialPower(Hero *) override;
     virtual std::vector<Item> getAdvanceRequirement() const override;
     virtual std::vector<Item> getDefeatRequirement() const override;
     virtual int getCounter() const override;
-    virtual bool isAdvanceLocation(const std::string&) const override;
+    virtual bool isAdvanceLocation(const std::string &) const override;
     virtual std::string serialize() const override;
-    void setCoffins(const std::map<std::string, bool>& newCoffins);
+    void setCoffins(const std::map<std::string, bool> &newCoffins);
 
+    bool isCoffinLocation(const std::string &locName) const;
+    bool isCoffinDestroyed(const std::string &locNamw) const;
 };
 
 #endif
