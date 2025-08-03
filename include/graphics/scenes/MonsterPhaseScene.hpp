992683d1e7f4e34f3f70b52bfb558171f7fa7db8
AiKhalili <aisakhalili760@gmail.com>
2025-08-03 09:25:21 +0330

    adding the moveAndRoll step to the hedear file

--- include/graphics/scenes/MonsterPhaseScene.hpp
+++ include/graphics/scenes/MonsterPhaseScene.hpp
@@ -14,7 +14,10 @@ enum class MonsterPhaseStep
     DrawMonsterCard,
     PlaceItems,
     RunEvent,
-    Move
+    MonsterMoveAndRoll,
+    HandleStrike,
+    HandlePower,
+    EndPhase
 };
 
 class MonsterPhaseScene : public Scene
@@ -24,7 +27,11 @@ private:
     Texture2D cardTexture;
     Texture2D bats;
     Texture2D villagerTex;
-    
+    Texture2D frenzy;
+    Texture2D diceBlank;
+    Texture2D diceStrike;
+    Texture2D dicePower;
+
     Font normalFont;
     Font spookyFont;
 
@@ -50,12 +57,23 @@ private:
     bool titelAdded = false;
     bool messageShown = false;
 
-    void showMessage(const std::string &text, Vector2 pos, int fontSize, float time);
+    std::vector<std::string> strikes;
+    bool processingStrike = false;
+
+    std::vector<std::string> rolledDiceResult;
+    float diceTimer = 0.0f;
+    int diceShown = 0;
+
+    int remainingStrikes = 0;
+
+    void showMessage(const std::string &text, Vector2 pos, int fontSize, float time, Font font, Color color = {235, 235, 235, 255});
 
     void step_CheckMonsterPhasePerk(float deleteTime);
     void step_DrawMonsterCard(float deleteTime);
     void step_PlaceItems(float deleteTime);
     void step_RunEvent(float deleteTime);
+    void step_MoveAndRoll(float deleteTime);
+    void step_HandleStrike(float deleteTime);
 
     void renderBackGround();
     void renderMonsterCard();
@@ -66,6 +84,17 @@ private:
     void render_Thief(float deleteTime);
     void render_Sunrise(float deleteTime);
     void render_villagerCard(float deleteTime);
+    void render_HypnoticGaze(float deleteTime);
+    void render_OnTheMove(float deleteTime);
+
+    void handleMoveAndRoll(float deleteTime);
+    void handleDefence(std::vector<Item *> &selectedItems);
+
+    void renderCurrentMonster();
+
+    void renderDice(float delerteTime);
+
+    void executeStrike();
 
 public:
     void onEnter() override;
