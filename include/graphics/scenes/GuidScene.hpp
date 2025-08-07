bf6764de496a214458e93c5c73be88dbab17a41d
ftmshab <ftmshbn76395.gmailcom>
2025-08-07 15:40:20 +0330

    Add Guid Action Scene

---
+++ include/graphics/scenes/GuidScene.hpp
@@ -0,0 +1,43 @@
+#ifndef GUIDSCENE_HPP
+#define GUIDSCENE_HPP
+
+#include "graphics/scenes/Scene.hpp"
+#include "graphics/ui/UIManager.hpp"
+#include "graphics/ui/UILabel.hpp"
+#include "core/Hero.hpp"
+#include "core/Game.hpp"
+#include "core/Location.hpp"
+#include "core/Villager.hpp"
+#include "core/SceneDataHub.hpp"
+#include "raylib.h"
+
+class GuidScene : public Scene
+{
+    
+private:
+    UIManager ui;
+    Texture2D background;
+    Font font;
+    Font errorFont;
+
+    void createLabels();
+    void createButtons();
+
+    Hero *hero;
+    Location *villloc;
+
+    Location *locselect;
+    std::vector<Villager *> villselect;
+
+    UILabel *errorLabel = nullptr;
+
+public:
+    void onEnter() override;
+    void onExit() override;
+    void update(float deltaTime) override;
+    void render() override;
+
+    void showErrorMessage(const std::string &msg);
+};
+
+#endif
