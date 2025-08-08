040d56aa19dfa547496437d0145a6ece06e0479a
AiKhalili <aisakhalili760@gmail.com>
2025-08-08 14:17:51 +0330

    adding the set and get skip func

--- include/graphics/scenes/Scene.hpp
+++ include/graphics/scenes/Scene.hpp
@@ -3,12 +3,18 @@
 
 class Scene
 {
+private:
+    bool skipExitEnter = false;
+
 public:
     virtual void update(float deleteTime) = 0;
     virtual void render() = 0;
     virtual void onEnter() {} // when you enter the scene
     virtual void onExit() {}  // when you exit the scene
     virtual ~Scene() = default;
+
+    void setSkip(bool skip) { skipExitEnter = skip; }
+    bool getSkip() const { return skipExitEnter; }
 };
 
 #endif
