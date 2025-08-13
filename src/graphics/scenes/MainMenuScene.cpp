34ac791bfdd1314ad2f894b20cecfe91c9e36ca6
AiKhalili <aisakhalili760@gmail.com>
2025-08-13 08:31:12 +0330

    change the background and buttons

--- src/graphics/scenes/MainMenuScene.cpp
+++ src/graphics/scenes/MainMenuScene.cpp
@@ -8,11 +8,9 @@
 void MainMenuScene::onEnter()
 {
     background = TextureManager::getInstance().getOrLoadTexture("main", "assets/images/background/main_menu.png");
-    titleFont = LoadFontEx("assets/fonts/spooky.otf", 100, 0, 0);
-    SetTextureFilter(titleFont.texture, TEXTURE_FILTER_BILINEAR);
     normalFont = LoadFont("assets/fonts/simple.ttf");
     AudioManager::getInstance().playMusic();
-    float x = GetScreenWidth() / 2 - 150;
+    float x = GetScreenWidth() / 2 - 200;
     float startY = 600;
     float width = 300; // width of button
     float height = 60; // height of button
@@ -21,7 +19,8 @@ void MainMenuScene::onEnter()
     // spacing between buttons
     auto makeButton = [&](const std::string &text, int index, std::function<void()> action) { // lambda function with access to outer variables
         Rectangle bounds = {x, startY + gap * index, width, height};
-        auto btn = std::make_unique<UIButton>(bounds, text, 32, WHITE, DARKGRAY, GRAY);
+        auto btn = std::make_unique<UIButton>(bounds, text, 32,
+                                              WHITE, BLACK, DARKGRAY, Color{239, 224, 197, 255});
         btn->setFont(normalFont);
         btn->setOnClick([action]()
                         {
@@ -37,7 +36,6 @@ void MainMenuScene::onEnter()
 
 void MainMenuScene::onExit()
 {
-    UnloadFont(titleFont);
     UnloadFont(normalFont);
     ui.clear();
 }
@@ -51,12 +49,10 @@ void MainMenuScene::update(float deleteTime)
 void MainMenuScene::render()
 {
     ClearBackground(BLACK);
-    DrawTexture(background, 0, 0, WHITE);
-    Vector2 textSize = MeasureTextEx(titleFont, "HORRIFIED", 100, 2);
-    float x = GetScreenWidth() / 2 - textSize.x / 2 - 70;
-    float y = 40;
-    Color color = {170, 20, 20, 255};
-    DrawTextEx(titleFont, "HORRIFIED", Vector2{x, y}, 150, 2, color);
+
+    float scaleX = 1600.0f / background.width;
+
+    DrawTextureEx(background, {0, 0}, 0.0f, scaleX, WHITE);
     ui.render();
 }
 
