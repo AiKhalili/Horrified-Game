f479ea47e628c89bdb6562f1cf8abc4af5b39c0c
ftmshab <ftmshbn76395.gmailcom>
2025-08-18 01:03:55 +0330

    Commenting and adding functions for saving and loading scenes and related items

--- include/graphics/scenes/PickUpScene.hpp
+++ include/graphics/scenes/PickUpScene.hpp
@@ -14,18 +14,17 @@ class PickUpScene : public Scene
 {
 
 private:
-    UIManager ui;
+    UIManager ui; // To control buttons and labels
     Texture2D background;
     Font font;
     Font errorFont;
 
-    Hero *hero;
-    std::vector<Item *> itemselect;
-    UILabel *errorLabel = nullptr;
+    Hero *hero;                     // Active hero during the game
+    std::vector<Item *> itemselect; // The chosen items
+    UILabel *errorLabel = nullptr;  // Labels to indicate various errors
 
-    void createLabels();
-    void createButtons();
-
+    void createLabels();  // Creating scene labels
+    void createButtons(); // Creatin scene buttons
 
 public:
     void onEnter() override;
@@ -33,6 +32,9 @@ public:
     void update(float deltaTime) override;
     void render() override;
 
-    void showErrorMessage(const std::string &msg);
+    void showErrorMessage(const std::string &msg); // Control the display of various errors
+
+    void serialize(const std::string &filename) override;
+    void deserialize(const std::string &filename) override;
 };
 #endif
