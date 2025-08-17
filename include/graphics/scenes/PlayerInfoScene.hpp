58d1b427b9f5b0d156981d617259cd98d26b3608
ftmshab <ftmshbn76395.gmailcom>
2025-08-18 01:05:00 +0330

    Commenting and arrange

--- include/graphics/scenes/PlayerInfoScene.hpp
+++ include/graphics/scenes/PlayerInfoScene.hpp
@@ -12,37 +12,37 @@
 
 class PlayerInfoScene : public Scene
 {
+private:
+    UIManager uiManager; // To control buttons and labels
+    Texture2D backgroundTexture;
+    Font font;
+    bool fontLoaded = false; // Flag to check if font is loaded
+
+    UIButton *continueButton = nullptr; // Button to submit player info and continue
+    UIButton *backButton = nullptr;     // Button to go back to main menu
+    UIButton *focusedButton = nullptr;  // Currently focused button
+
+    UILabel *errorLabel = nullptr; // Currently focused button
+
+    UIInputBox *nameInput = nullptr; // Input box for player's name
+    UIInputBox *timeInput = nullptr; // Input box for last garlic-eating time
+
+    UILabel *playerLabel = nullptr; // Label showing which player is entering info
+
+    int currentPlayer = 1; // Current player index (1 or 2)
+
+    void createButtons();    // Creating scene labels
+    void createInputBoxes(); // Creates the input boxes for name and time
+    void createLabels();     // Creatin scene buttons
+
 public:
     void onEnter() override;
     void onExit() override;
     void update(float deltaTime) override;
     void render() override;
 
-    void resetInputsForNextPlayer();
-    void showErrorMessage(const std::string &msg);
-
-private:
-    UIManager uiManager;
-    Texture2D backgroundTexture;
-    Font font;
-    bool fontLoaded = false;
-
-    UIButton *continueButton = nullptr;
-    UIButton *backButton = nullptr;
-    UIButton *focusedButton = nullptr;
-
-    UILabel* errorLabel = nullptr;
-
-    UIInputBox *nameInput = nullptr;
-    UIInputBox *timeInput = nullptr;
-
-    UILabel* playerLabel = nullptr;
-
-    int currentPlayer = 1;
-
-    void createButtons();
-    void createInputBoxes();
-    void createLabels();
+    void resetInputsForNextPlayer();               // Clears the input boxes for the next player
+    void showErrorMessage(const std::string &msg); // Control the display of various errors
 };
 
 #endif
