8aa3c2188c2ae70f7e3f57d69c909e77329db1b9
ftmshab <ftmshbn76395.gmailcom>
2025-08-18 01:05:40 +0330

    Commenting and adding functions for saving and loading scenes and related items

--- include/graphics/scenes/PlayerSummaryScene.hpp
+++ include/graphics/scenes/PlayerSummaryScene.hpp
@@ -10,28 +10,34 @@ class PlayerSummaryScene : public Scene
 {
 
 private:
-    UIManager ui;
+    UIManager ui; // To control buttons and labels
     Texture2D background;
     Font Titlefont;
     Font Simplefont;
-    std::string p1Name, p2Name, h1Name, h2Name;
-    Texture2D hero1Texture;
-    Texture2D hero2Texture;
 
-    UILabel *errorLabel = nullptr;
+    std::string p1Name, p2Name, h1Name, h2Name; // Names of players and heroes
+    Texture2D hero1Texture;                     // The image of the first hero
+    Texture2D hero2Texture;                     // The image of the second hero
 
-    void createLabels();
-    void creatButtons();
-    void LoadHeroTexturs();
+    UILabel *errorLabel = nullptr; // Labels to indicate various errors
+
+    void createLabels();    // Creating scene labels
+    void creatButtons();    // Creatin scene buttons
+    void LoadHeroTexturs(); // Loads hero textures based on their names
 
 public:
     void onEnter() override;
     void onExit() override;
     void update(float deltaTime) override;
     void render() override;
+
     void setData(const std::string &player1Name, const std::string &hero1,
                  const std::string &player2Name, const std::string &hero2);
-    void showErrorMessage(const std::string &msg);
     std::vector<std::string> getData();
+
+    void showErrorMessage(const std::string &msg); // Control the display of various errors
+
+    void serialize(const std::string &filename) override;
+    void deserialize(const std::string &filename) override;
 };
 #endif
