6656d1675418e3c8266203efc116c49ef8d1cfbd
ftmshab <ftmshbn76395.gmailcom>
2025-08-18 01:10:22 +0330

    Commenting and adding functions for saving and loading scenes and related items

--- include/saves/SaveManager.hpp
+++ include/saves/SaveManager.hpp
@@ -1,6 +1,5 @@
 #ifndef SAVEMANAGER_HPP
 #define SAVEMANAGER_HPP
-
 #include <string>
 #include <vector>
 #include <ctime>
@@ -25,63 +24,58 @@ class Map;
 
 class SaveManager
 {
-
 private:
-
     SaveManager() = default;
 
-    SaveManager(const SaveManager&) = delete;
-    SaveManager& operator=(const SaveManager&) = delete;
+    SaveManager(const SaveManager &) = delete;
+    SaveManager &operator=(const SaveManager &) = delete;
+
+    std::vector<Item *> ItemInter;
 
 public:
-    static SaveManager& getInstance();
+    static SaveManager &getInstance();
 
-    void saveGameToSlot(std::string newkey); // ذخیره کامل بازی با شماره اسلات
-    void loadGameFromSlot(int); // بارگذاری بازی با شماره اسلاتی که کاربر وارد کرده و میخواد بازی رو ادامه بده
+    std::vector<Item *>& getItemInter();
 
-    void saveHeroesToFile(const std::string&, const std::vector<Hero*>& ); // ذخیره کردن قهرمان فعال بازی
-    std::vector<Hero*> loadHeroesFromFile(const std::string&); // خواندن اطلاعات قهرمان و ساختن آن بر اساس اطاعات قبلی برای ادامه بازی
+    void saveGameToSlot(std::string newkey); // Full game save according to the scene we saved
+    void loadGameFromSlot(int);              // Load the game with the slot number that the user entered and wants to continue the game
 
-    void saveCurrentHeroStateToFile(const std::string& , int, int);
-    std::pair<int, int> loadCurrentHeroStateFromFile(const std::string&);
-    
-    void saveMonstersToFile(const std::string&, const std::vector<Monster*>&);
-    std::vector<Monster*> loadMonstersFromFile(const std::string&);
+    void saveHeroesToFile(const std::string &, const std::vector<Hero *> &); // Save the heros in the game
+    std::vector<Hero *> loadHeroesFromFile(const std::string &);             // Reading and loading information for each hero
 
-    void saveVillagersToFile(const std::string&, const std::vector<Villager*>&);
-    std::vector<Villager*> loadVillagersFromFile(const std::string&);
+    void saveCurrentHeroStateToFile(const std::string &, int, int);        // Save the active hero in the game
+    std::pair<int, int> loadCurrentHeroStateFromFile(const std::string &); // Reading hero information and building it based on previous information to continue the game
 
-    void saveItemsToFile(const std::string&, const std::vector<Item*>&);
-    std::vector<Item*> loadItemsFromFile(const std::string&);
+    void saveMonstersToFile(const std::string &, const std::vector<Monster *> &); // Save each monster and its information
+    std::vector<Monster *> loadMonstersFromFile(const std::string &);             // Reading each monster's information and loading it
 
-    void saveMonsterCardsToFile(const std::string& , const std::vector<MonsterCard>&);
-    std::vector<MonsterCard> loadMonsterCardsFromFile(const std::string&);
+    void saveVillagersToFile(const std::string &, const std::vector<Villager *> &); // Save the villagers in the game and information about them
+    std::vector<Villager *> loadVillagersFromFile(const std::string &);             // Reading each villager's information and loading it
 
-    void saveItemManagerStateToFile(const std::string&);
-    void loadItemManagerStateFromFile(const std::string&);
+    void saveItemsToFile(const std::string &, const std::vector<Item *> &); // Save the items in the game and information about them
+    std::vector<Item *> loadItemsFromFile(const std::string &);             // Reading each items's information and loading it
 
-    void saveTerrorLevel(const std::string&, int);
-    int loadTerrorLevel(const std::string&);
+    void saveMonsterCardsToFile(const std::string &, const std::vector<MonsterCard> &); // Save the monster cards and information about them
+    std::vector<MonsterCard> loadMonsterCardsFromFile(const std::string &);             // Load the monster cards
 
-    void saveCurrentTurn(const std::string&, int);
-    int loadCurrentTurn(const std::string&);
+    void saveItemManagerStateToFile(const std::string &);   // Save the current state of the ItemManager to a file, This includes the bag contents and used items
+    void loadItemManagerStateFromFile(const std::string &); // Load the ItemManager state from a file, Reads bag contents and used items
 
-    void saveTimes(const std::string&, int, int);
-    void loadTimes(const std::string&, int&, int&);
+    void saveTerrorLevel(const std::string &, int); // Save the Terror Level at that stage of the game
+    int loadTerrorLevel(const std::string &);       // Loading horror levels during gameplay
 
-    void saveFrenzy(const std::string& , const std::string&);
-    Monster* loadFrenzy(const std::string&, const std::vector<Monster*>&);
+    void saveFrenzy(const std::string &, const std::string &);                // Save the current Frenzy monster's name to a file
+    Monster *loadFrenzy(const std::string &, const std::vector<Monster *> &); // Load the Frenzy monster from a file
 
-    void savePerkDeckToFile(const std::string&, const std::vector<PerkCard>&);
-    std::vector<PerkCard> loadPerkDeckFromFile(const std::string&);
+    void savePerkDeckToFile(const std::string &, const std::vector<PerkCard> &); // Save the perk cards and information about them
+    std::vector<PerkCard> loadPerkDeckFromFile(const std::string &);             // Load the perk cards
 
-    void writeSaveTimestamp(const std::string&);
-    std::string readSaveTimestamp(const std::string& );
+    void writeSaveTimestamp(const std::string &);       // Save game save date and time
+    std::string readSaveTimestamp(const std::string &); // Load game save date and time
 
-    std::vector<std::pair<int, std::string>> getAllSaveSlotsWithTimestamps();
+    std::vector<std::pair<int, std::string>> getAllSaveSlotsWithTimestamps(); // Retrieve all available save slots along with their timestamps
 
-    void saveSceneToFile(std::string& filename, std::string scenekey);
-    void loadSceneFromFile(const std::string&);
+    void saveAllScenesToFile(const std::string &filename,const std::string &scenekey); // Save all the scene information from where we saved the game
+    void loadAllScenesFromFile(const std::string &);                       // Loading all the scene information from where we saved the game to continue the game from there
 };
-
 #endif
