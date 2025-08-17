dc199a0e7833ee21ed80d72042f4f367c2b641ad
ftmshab <ftmshbn76395.gmailcom>
2025-08-18 00:58:50 +0330

    Commenting and adding functions for saving and loading scenes and related items

--- include/graphics/scenes/MonsterSelectionScene.hpp
+++ include/graphics/scenes/MonsterSelectionScene.hpp
@@ -12,37 +12,44 @@ class Game;
 class MonsterSelectionScene : public Scene
 {
 private:
-    UIManager ui;
+    UIManager ui; // To control buttons and labels
     Texture2D background;
     Font font;
+    std::string scenekey; // Scene key to return to after selection
 
-    Texture2D monsterTextures[2];
-    void LoadeMonsterTextures();
-    void createButtons();
-    void createLabels();
+    UILabel *errorLabel = nullptr; // Label for showing error messages
 
-    int hoverMonster = -1;
-    int clickedMonster = -1; 
-    int confirmedMonster = -1;
-    Monster* selectedMonster = nullptr;
-    std::vector<Monster *> monsters;
-    std::vector<std::string> monsterNames;
-    std::string clickedMonsterName = "";
-    
-    std::string scenekey;
+    Texture2D monsterTextures[2]; // Loaded textures for each monster
 
-    UILabel *errorLabel = nullptr;
+    int hoverMonster = -1;                 // Index of currently hovered monster
+    int clickedMonster = -1;               // Index of currently clicked monster
+    int confirmedMonster = -1;             // Index of confirmed monster
+    Monster *selectedMonster = nullptr;    // Selected monster pointer
+    std::vector<Monster *> monsters;       // Monsters available for selection.
+    std::vector<std::string> monsterNames; // Names of the monsters
+    std::string clickedMonsterName = "";   // Name of the monster clicked by player
+
+    bool firstDeserialize = true; 
+
+    void LoadeMonsterTextures(); // Loads textures for all monster in the scene
+    void createButtons();        // Creates buttons in the scene
+    void createLabels();         // Creating page labels and specifications for each monster
 
 public:
     void onEnter() override;
     void onExit() override;
     void update(float deltaTime) override;
     void render() override;
+
     void setDate(const std::vector<Monster *> &Monsters, const std::string &newkey);
-    Monster* getSelectedMonster();
-    void showErrorMessage(const std::string &msg);
+    Monster *getSelectedMonster();
+
+    void showErrorMessage(const std::string &msg); // Show error message on screen.
+
     std::vector<Monster *> getMonsters();
     std::string getscenekey();
-};
 
+    void serialize(const std::string &filename) override;
+    void deserialize(const std::string &filename) override;
+};
 #endif
